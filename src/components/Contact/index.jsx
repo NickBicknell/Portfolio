05bc2ef4e218a1850192f17e381d087f684717e5
Nick Bicknell <nicholasbicknell@gmail.com>
2023-11-01 18:09:57 -0500

    updated contact inputs to useState

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 
 function Contact() {
+
+  const [ contactName, setContactName ] = useState('')
+  const [ contactEmail, setContactEmail ] = useState('')
+  const [ contactSubject, setContactSubject ] = useState('')
+  const [ contactMessage, setContactMessage ] = useState('')
 
   function sendEmail(e) {
     e.preventDefault();
@@ -10,18 +15,11 @@ function Contact() {
       .sendForm("service_gd267mb", "template_4iik70o", e.target, "KJh3qHLSfN9B91nQU")
       .then(
         (result) => {
-          document
-            .querySelector('#contact-name')
-            .value = ''
-          document
-            .querySelector('#contact-email')
-            .value = ''
-          document
-            .querySelector('#contact-subject')
-            .value = ''
-          document
-            .querySelector('#contact-message')
-            .value = ''
+          console.log('success')
+          setContactName('')
+          setContactEmail('')
+          setContactSubject('')
+          setContactMessage('')
         },
         (error) => {
           console.log(error.text);
@@ -38,7 +36,7 @@ function Contact() {
     <div className="container mb-4 pb-5">
       <div className="row justify-content-center">
         <div className="col-6 col-lg-3">
-          <h1 className="display-2 text-white text-decoration-underline pb-3">
+          <h1 className="display-2 text-white pb-3">
             Contact
           </h1>
         </div>
@@ -47,17 +45,19 @@ function Contact() {
         <form className="contact-card col-11 col-lg-6" onSubmit={sendEmail}>
           <div className="card-body mb-3">
             <label className="form-label contact-form">Name</label>
-            <input type="text" className="form-control" id="contact-name" name="from_name" />
+            <input type="text" className="form-control" id="contact-name" name="from_name" value={contactName} onChange={(e) => setContactName(e.target.value)} />
             <label className="form-label contact-form pt-3">Email address</label>
-            <input type="email" className="form-control" id="contact-email" name="from_email" />
+            <input type="email" className="form-control" id="contact-email" name="from_email" value={contactEmail} onChange={(e) => setContactEmail(e.target.value)} />
             <label className="form-label pt-3 contact-form">Subject</label>
-            <input type="text" className="form-control" id="contact-subject" name="subject" />
+            <input type="text" className="form-control" id="contact-subject" name="subject" value={contactSubject} onChange={(e) => setContactSubject(e.target.value)} />
           </div>
           <div className="mb-3">
             <label className="form-label contact-form">Message</label>
             <textarea
               className="form-control"
               id="contact-message"
+              value={contactMessage} 
+              onChange={(e) => setContactMessage(e.target.value)}
               name="message"
               rows="5"
             ></textarea>
